52417883478e8249a2add5aa399463b6cddd2113
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2019-09-22 12:31:57 +1000

    adding show method to api

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->resource('inverter-log', 'InverterLogController', ['only' => 'store']);
+Route::middleware('auth:api')->resource('inverter-log', 'InverterLogController', ['only' => ['store', 'show']]);
